0fb9cdf82aca613572575eb875f0db6ac667cf73
carlos47 <carlosidriss46@gmail.com>
2026-07-18 19:11:02 +0200

    Update admin.py

--- apps/forums/admin.py
+++ apps/forums/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from .models import ForumCategorie, ForumSujet, ForumMessage
-from .models import CategorieForum
 
-@admin.register(CategorieForum)
-class CategorieForumAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('titre',)}
-    list_display = ('id', 'titre')
 
 @admin.register(ForumCategorie)
 class ForumCategorieAdmin(admin.ModelAdmin):
@@ -13,11 +8,13 @@ class ForumCategorieAdmin(admin.ModelAdmin):
     list_editable = ('ordre', 'est_active')
     search_fields = ('nom',)
 
+
 @admin.register(ForumSujet)
 class ForumSujetAdmin(admin.ModelAdmin):
     list_display = ('titre', 'categorie', 'auteur', 'date_creation')
     list_filter = ('categorie',)
     search_fields = ('titre', 'contenu')
+
 
 @admin.register(ForumMessage)
 class ForumMessageAdmin(admin.ModelAdmin):
