b67caca5d94cb66cc4555dc53c5e26a857d07058
carlos47 <carlosidriss46@gmail.com>
2026-07-19 10:55:47 +0200

    Update urls.py

--- apps/authentification/urls.py
+++ apps/authentification/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.views.generic import RedirectView
 from . import views
 
 app_name = 'authentification'
 
 urlpatterns = [
     path('configurer/', views.configurer, name='configurer'),
-    path('inscription-privee/', RedirectView.as_view(pattern_name='membres:adhesion', permanent=False), name='inscription_privee'),
+    path('inscription-privee/', views.inscription_privee, name='inscription_privee'),
 ]
