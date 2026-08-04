6385b9b50393cf877877d749c8c5f4fb187ca4b1
carlos47 <carlosidriss46@gmail.com>
2026-08-04 20:48:23 +0200

    Update urls.py

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.liste, name='liste'),
     path('marquer-toutes-lues/', views.marquer_toutes_lues, name='marquer_toutes_lues'),
     path('preferences/', views.preferences, name='preferences'),
+    path('messagerie/', views.messagerie_admin, name='messagerie_admin'),
+    path('messagerie/<int:membre_id>/', views.messagerie_admin, name='messagerie_admin_membre'),
+    path('mes-messages/', views.messagerie_membre, name='messagerie_membre'),
 ]
