578c6b60962ad5b15b94773cfa22246250de52f7
carlos47 <carlosidriss46@gmail.com>
2026-08-12 16:42:20 +0200

    Update models.py

--- apps/demandes/models.py
+++ apps/demandes/models.py
@@ -4,9 +4,9 @@ Modèles pour la gestion des demandes des entités
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
-
-
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+
 
 def valider_fichier_demande(fichier):
     extensions_autorisees = ['.pdf', '.doc', '.docx']
@@ -16,6 +16,7 @@ def valider_fichier_demande(fichier):
     taille_max_mo = 5
     if fichier.size > taille_max_mo * 1024 * 1024:
         raise ValidationError(f'Le fichier ne doit pas dépasser {taille_max_mo} Mo.')
+
 
 class Demande(models.Model):
     """
@@ -27,14 +28,14 @@ class Demande(models.Model):
         ('invest', 'BETA-Résilience INVEST'),
         ('laboratoire', 'Laboratoire Résilience'),
     ]
-    
+
     STATUT_CHOICES = [
         ('en_attente', 'En attente'),
         ('en_cours', 'En cours de traitement'),
         ('traite', 'Traité'),
         ('rejete', 'Rejeté'),
     ]
-    
+
     # Types de demande par entité
     TYPE_CHOICES = {
         'association': [
@@ -65,26 +66,36 @@ class Demande(models.Model):
             ('partenariat', 'Partenariat académique'),
         ],
     }
-    
+
+    # Lien éventuel avec un utilisateur ou un membre (autorise la suppression sans erreur d'intégrité)
+    utilisateur = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='demandes_entites',
+        verbose_name='Utilisateur associé'
+    )
+
     # Informations générales
     entite = models.CharField('Entité concernée', max_length=20, choices=ENTITE_CHOICES)
     type_demande = models.CharField('Type de demande', max_length=50)
     autre_type = models.CharField('Autre type (précisez)', max_length=100, blank=True)
-    
+
     # Informations personnelles
     nom = models.CharField('Nom', max_length=100)
     prenom = models.CharField('Prénom', max_length=100)
     email = models.EmailField('Email')
     telephone = models.CharField('Téléphone', max_length=50)
-    
+
     # Informations professionnelles
     societe = models.CharField('Société / Institution', max_length=200, blank=True)
     fonction = models.CharField('Fonction', max_length=100, blank=True)
-    
+
     # Message
     objet = models.CharField('Objet de la demande', max_length=200)
     message = models.TextField('Message')
-    
+
     # Pièce jointe (optionnelle)
     fichier = models.FileField(
         'Document joint',
@@ -93,31 +104,31 @@ class Demande(models.Model):
         null=True,
         validators=[valider_fichier_demande],
     )
-    
+
     # Statut
     statut = models.CharField('Statut', max_length=20, choices=STATUT_CHOICES, default='en_attente')
     commentaire_admin = models.TextField('Commentaire de l\'administrateur', blank=True)
-    
+
     # IP et métadonnées
     ip_address = models.GenericIPAddressField('Adresse IP', null=True, blank=True)
     user_agent = models.CharField('User Agent', max_length=500, blank=True)
-    
+
     # Timestamps
     date_soumission = models.DateTimeField('Date de soumission', auto_now_add=True)
     date_traitement = models.DateTimeField('Date de traitement', null=True, blank=True)
     date_modification = models.DateTimeField('Date de modification', auto_now=True)
-    
+
     class Meta:
         verbose_name = 'Demande'
         verbose_name_plural = 'Demandes'
         ordering = ['-date_soumission']
-    
+
     def __str__(self):
         return f'{self.get_entite_display()} - {self.prenom} {self.nom} ({self.get_statut_display()})'
-    
+
     def get_absolute_url(self):
         return reverse('demandes:detail', kwargs={'pk': self.pk})
-    
+
     def get_type_demande_display(self):
         """Retourner le libellé du type de demande"""
         types = self.TYPE_CHOICES.get(self.entite, [])
@@ -125,15 +136,15 @@ class Demande(models.Model):
             if value == self.type_demande:
                 return label
         return self.type_demande
-    
+
     def get_type_choices(self):
         """Retourner les types de demande pour l'entité sélectionnée"""
         return self.TYPE_CHOICES.get(self.entite, [])
-    
+
     @property
     def nom_complet(self):
         return f'{self.prenom} {self.nom}'
-    
+
     @property
     def entite_icone(self):
         """Retourner l'icône de l'entité"""
@@ -144,7 +155,7 @@ class Demande(models.Model):
             'laboratoire': 'fa-flask',
         }
         return icons.get(self.entite, 'fa-question-circle')
-    
+
     @property
     def entite_couleur(self):
         """Retourner la couleur de l'entité"""
@@ -155,7 +166,7 @@ class Demande(models.Model):
             'laboratoire': '#6A1B9A',
         }
         return colors.get(self.entite, '#757575')
-    
+
     @property
     def entite_badge(self):
         """Retourner le badge HTML de l'entité"""
